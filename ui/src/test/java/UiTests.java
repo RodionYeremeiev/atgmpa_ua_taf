f02fd6f37a1396b834b97703c79ef9affad476bd
RodionY <rodion.yeremeiev@lseg.com>
2023-04-20 23:26:49 +0300

    some changes

--- ui/src/test/java/UiTests.java
+++ ui/src/test/java/UiTests.java
@@ -1,5 +1,6 @@
+import static com.codeborne.selenide.Selenide.open;
+
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,6 @@ public class UiTests {
     @DisplayName("UI test Stub")
     public void test(){
         log.info("UI TEST MESSAGE");
-        Assertions.assertEquals(1,1, "TestStub");
+        open("https://rp.epam.com");
     }
 }
